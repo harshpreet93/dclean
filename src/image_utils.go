d05f8c7cafb1fad6e58e9e7ddb632edb5cf1ed5e
Harshpreet Singh <singhharshpreet1993@gmail.com>
2016-11-02 23:40:29 -0700

    filter based on image repo tags

--- src/image_utils.go
+++ src/image_utils.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/fsouza/go-dockerclient"
-	//"time"
+	"time"
+	"regexp"
 )
 
 func getAllImgs() []docker.APIImages {
@@ -17,25 +18,36 @@ func getAllImgs() []docker.APIImages {
 		panic(err)
 	}
 	return imgs
-	//for _, img := range imgs {
-	//	//i, _ := strconv.ParseInt(img.Created, 10, 64)
-	//
-	//	fmt.Println("ID: ", img.ID)
-	//	fmt.Println("RepoTags: ", img.RepoTags)
-	//	fmt.Println("Created: ", time.Unix(img.Created, 0))
-	//	fmt.Println("Size: ", img.Size)
-	//	fmt.Println("VirtualSize: ", img.VirtualSize)
-	//	fmt.Println("ParentId: ", img.ParentID)
-	//}
 }
 
-func imgsWithTag(imgs []docker.APIImages, tagRegex string) {
-	//fmt.Println()
-	
+func printImg(img docker.APIImages) {
+	fmt.Println("ID: ", img.ID)
+	fmt.Println("RepoTags: ", img.RepoTags)
+	fmt.Println("Created: ", time.Unix(img.Created, 0))
+	fmt.Println("Size: ", img.Size)
+	fmt.Println("VirtualSize: ", img.VirtualSize)
+	fmt.Println("ParentId: ", img.ParentID)
+}
+
+func printImgs(imgs []docker.APIImages)  {
+	for _, img := range imgs {
+		printImg(img)
+	}
+}
+
+func imgsWithTag(imgs []docker.APIImages, tagRegex string) []docker.APIImages {
+	var imgsWithTag []docker.APIImages
+	for _, img := range imgs {
+		matched, _ := regexp.MatchString(tagRegex, img.RepoTags[0])
+		if matched {
+			imgsWithTag = append(imgsWithTag, img)
+		}
+	}
+	return imgsWithTag
 }
 
 
 func main() {
-	fmt.Println("hello, world")
-	getAllImgs()
+	printImgs( imgsWithTag( getAllImgs(), "^cent.*" ) )
+
 }
